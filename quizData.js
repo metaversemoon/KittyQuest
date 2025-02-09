69297a10787582e59a0e4583ee3a20628951f8ee
metaversemoon <99382573+metaversemoon@users.noreply.github.com>
2025-02-09 11:44:28 -0500

    add config

---
+++ quizData.js
@@ -0,0 +1,38 @@
+export const quizData = {
+  author: "0x021B278a90fC4764e97d00c73966e36b",
+  title: "Cat 101",
+  max_Points: "100",
+  description: "This survey aims to provide information and insights about cats.",
+  questions: [
+    {
+      question: "What is a common sign of a healthy cat?",
+      options: [
+        { a: "Shiny coat and bright eyes" },
+        { b: "Constant sneezing" },
+        { c: "Lack of appetite" },
+        { d: "Scratching excessively" },
+      ],
+      correct: "a",
+    },
+    {
+      question: "Which cat breed is known for its long, luxurious fur?",
+      options: [{ a: "Siamese" }, { b: "Persian" }, { c: "Sphynx" }, { d: "Bengal" }],
+      correct: "b",
+    },
+    {
+      question: "What is a key ingredient in a balanced cat food diet?",
+      options: [{ a: "Chocolate" }, { b: "Caffeine" }, { c: "Taurine" }, { d: "Onions" }],
+      correct: "c",
+    },
+    {
+      question: "How often should you take your cat to the veterinarian for a checkup?",
+      options: [{ a: "Once every 5 years" }, { b: "Only when sick" }, { c: "Every 6 months" }, { d: "Once a year" }],
+      correct: "d",
+    },
+    {
+      question: "Which cat breed is known for its lack of fur?",
+      options: [{ a: "Maine Coon" }, { b: "Ragdoll" }, { c: "Sphynx" }, { d: "Scottish Fold" }],
+      correct: "c",
+    },
+  ],
+};
